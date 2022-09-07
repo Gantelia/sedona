19fa65b01227e177d9aa3df1003084c5f220beba
Gantelia <helengriva@gmail.com>
2022-09-07 20:19:32 +0300

    Устранит ошибку в script.js, добавит alt картинкам каталога

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,12 @@
 const navigation = document.querySelector('.main-nav');
 const toggleNavigation = navigation.querySelector('.main-nav__toggle');
+const form = document.querySelector('.form');
+const modal = document.querySelector('.page__modal-container');
+const closeModalButton = modal?.querySelector('.modal__button');
 
 navigation.classList.remove('main-nav--nojs');
+
+// Управление навигацией
 
 toggleNavigation.addEventListener('click', () => {
   if (navigation.classList.contains('main-nav--closed')) {
@@ -11,4 +16,16 @@ toggleNavigation.addEventListener('click', () => {
     navigation.classList.remove('main-nav--open');
     navigation.classList.add('main-nav--closed');
   }
+})
+
+
+// Управление модальным окном (демоверсия)
+
+form?.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  modal.classList.add('page__modal-container--open');
+})
+
+closeModalButton?.addEventListener('click', () => {
+  modal.classList.remove('page__modal-container--open');
 })
